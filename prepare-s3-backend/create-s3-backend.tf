f9e6350bc05c1478d44f35a6aef8d91c0ab56d95
Alex <alex.burym@itechart-group.com>
2021-01-26 19:07:53 +0300

    Add a few more rules to public restriction s3 policy

--- prepare-s3-backend/create-s3-backend.tf
+++ prepare-s3-backend/create-s3-backend.tf
@@ -32,6 +32,8 @@ resource "aws_kms_key" "state_key" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.terraform-state.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
