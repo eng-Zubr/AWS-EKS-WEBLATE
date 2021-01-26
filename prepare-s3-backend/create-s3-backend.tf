0969f411e3758f7de7e05331ed7e5c203d543443
Alex <alex.burym@itechart-group.com>
2021-01-26 18:48:35 +0300

    Add state bucket encryption

--- prepare-s3-backend/create-s3-backend.tf
+++ prepare-s3-backend/create-s3-backend.tf
@@ -9,5 +9,22 @@ resource "aws_s3_bucket" "terraform-state" {
     prevent_destroy = true
   }
 
-  tags = {Name = "S3 Remote Terraform State Store"}
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.state_key.arn
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name = "S3 Remote Terraform State Store"
+  }
+
+}
+
+resource "aws_kms_key" "state_key" {
+  description = "This key is used to encrypt bucket objects - TERRAFORM STATE"
+  deletion_window_in_days = 10
 }
